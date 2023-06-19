80442c7477e91ad902a57620c07f198818a4205d
luigimariapascali <luigimariapascali@gmail.com>
2023-06-19 22:55:11 +0200

    JS first version complete

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
+// PRIMA VERSIONE: 
+
+// Definisco la lista della spesa come un array
+const listaSpesa = ["mele", "pane", "latte", "uova", "formaggio"];
+
+// Inizializzazione variabile 
+let i = 0;
+
+// Iterazione attraverso l'array della lista della spesa utilizzando un ciclo while
+while (i < listaSpesa.length) {
+// Stampo l'elemento corrente della lista della spesa sulla console
+console.log(listaSpesa[i]);
+
+// Incremento la variabile contatore
+i++;
+}
 
 
 
